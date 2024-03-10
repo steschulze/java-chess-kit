830edb42170b47da6fb200595ea2c693d8fb5745
Stefan Schulze <stefan.schulze@siemens.com>
2024-03-10 20:29:58 +0100

    Add shortName(), fullName() and fromSymbol() to Color.java

--- src/core/Color.java
+++ src/core/Color.java
@@ -7,4 +7,18 @@ public enum Color {
         if (this == WHITE) return BLACK;
         else return WHITE;
     }
+
+    String shortName(){
+        return name().toLowerCase().substring(0, 1);
+    }
+
+    String fullName(){
+        return name().toLowerCase();
+    }
+
+    static Color fromSymbol(char symbol){
+        if(symbol == 'w') return WHITE;
+        else if (symbol == 's') return BLACK;
+        else throw new IllegalArgumentException("No Color for symbol " + symbol);
+    }
 }
