0902901632cba433c8b62420c667c28d85b5c614
Stefan Schulze <stefan.schulze@mdcc-fun.de>
2024-03-13 12:26:34 +0100

    Add further tests to ColorTest.java
    - add test methods for fromSymbol() and fullName()

--- src/test/java/de/stefanet/javachesskit/core/ColorTest.java
+++ src/test/java/de/stefanet/javachesskit/core/ColorTest.java
@@ -3,6 +3,7 @@ package de.stefanet.javachesskit.core;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ColorTest {
 
@@ -10,6 +11,19 @@ class ColorTest {
     void testOther() {
         assertEquals(Color.BLACK, Color.WHITE.other());
         assertEquals(Color.WHITE, Color.BLACK.other());
+    }
+
+    @Test
+    void testFromSymbol() {
+        assertEquals(Color.WHITE, Color.fromSymbol('w'));
+        assertEquals(Color.BLACK, Color.fromSymbol('b'));
+        assertThrows(IllegalArgumentException.class, () -> Color.fromSymbol('W'));
+    }
+
+    @Test
+    void testFullName() {
+        assertEquals("white", Color.WHITE.fullName());
+        assertEquals("black", Color.fromSymbol('b').fullName());
     }
 
 }
