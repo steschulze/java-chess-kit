cc628e8691273ef39341315d9317aa25585f9458
Stefan Schulze <stefan.schulze@mdcc-fun.de>
2024-03-15 16:35:51 +0100

    Add Javadoc to InvalidFENException.java

--- src/main/java/de/stefanet/javachesskit/core/InvalidFENException.java
+++ src/main/java/de/stefanet/javachesskit/core/InvalidFENException.java
@@ -1,6 +1,15 @@
 package de.stefanet.javachesskit.core;
 
+/**
+ * Exception thrown when an invalid FEN (Forsyth&ndash;Edwards Notation) string is encountered.
+ */
 public class InvalidFENException extends RuntimeException {
+
+	/**
+	 * Constructs an InvalidFENException with the specified detail message.
+	 *
+	 * @param msg The detail message.
+	 */
 	public InvalidFENException(String msg) {
 		super(msg);
 	}
