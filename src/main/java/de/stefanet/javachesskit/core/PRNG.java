8273d3a8c7580eaf3861aa8d7026000fa277930d
Stefan Schulze <stefan.schulze@mdcc-fun.de>
2024-03-15 16:46:08 +0100

    Add Javadoc to PRNG.java

--- src/main/java/de/stefanet/javachesskit/core/PRNG.java
+++ src/main/java/de/stefanet/javachesskit/core/PRNG.java
@@ -1,8 +1,16 @@
 package de.stefanet.javachesskit.core;
 
+/**
+ * Pseudo-random number generator (PRNG) based on xorshift128* algorithm.
+ */
 public class PRNG {
 	private long s;
 
+	/**
+	 * Generates a 64-bit random number.
+	 *
+	 * @return A 64-bit random number.
+	 */
 	public long rand64() {
 		s ^= s >>> 12;
 		s ^= s << 25;
@@ -10,6 +18,12 @@ public class PRNG {
 		return s * 2685821657736338717L;
 	}
 
+	/**
+	 * Constructs a PRNG with the specified seed.
+	 *
+	 * @param seed The seed value for initializing the PRNG.
+	 * @throws IllegalArgumentException If the seed is zero.
+	 */
 	public PRNG(long seed) {
 		if (seed == 0) {
 			throw new IllegalArgumentException("Seed must not be zero.");
