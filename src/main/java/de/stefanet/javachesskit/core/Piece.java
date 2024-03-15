134a6274e9eff83fa214a8655911f9867b501d82
Stefan Schulze <stefan.schulze@mdcc-fun.de>
2024-03-15 17:20:54 +0100

    Add Javadoc to Piece.java

--- src/main/java/de/stefanet/javachesskit/core/Piece.java
+++ src/main/java/de/stefanet/javachesskit/core/Piece.java
@@ -3,10 +3,20 @@ package de.stefanet.javachesskit.core;
 
 import java.util.Objects;
 
+/**
+ * Represents chess pieces consisting of a PieceType and a Color.
+ */
 public class Piece {
     private final PieceType type;
     private final Color color;
 
+    /**
+     * Constructs a Piece based on the given symbol.
+     * An uppercase symbol is used for the white pieces, a lowercase symbol for the black pieces.
+     *
+     * @param symbol The symbol representing the piece.
+     * @throws IllegalArgumentException If the symbol is invalid.
+     */
     public Piece(char symbol) {
         if ("pnbrkqPNBRKQ".indexOf(symbol) == -1) {
             throw new IllegalArgumentException("Invalid piece symbol: " + symbol);
@@ -18,23 +28,51 @@ public class Piece {
         this.color = lowerCaseSymbol == symbol ? Color.BLACK : Color.WHITE;
     }
 
+    /**
+     * Constructs a Piece with the specified type and color.
+     *
+     * @param type  The type of the piece.
+     * @param color The color of the piece.
+     */
     private Piece(PieceType type, Color color) {
         this.type = type;
         this.color = color;
     }
 
+    /**
+     * Creates a Piece from the given type and color.
+     *
+     * @param type  The {@link PieceType} of the piece.
+     * @param color The {@link Color} of the piece.
+     * @return A Piece object with the specified type and color.
+     */
     public static Piece fromTypeAndColor(PieceType type, Color color) {
         return new Piece(type, color);
     }
 
+    /**
+     * Gets the type of the piece.
+     *
+     * @return The type of the piece.
+     */
     public PieceType getType() {
         return type;
     }
 
+    /**
+     * Gets the color of the piece.
+     *
+     * @return The color of the piece.
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Gets the symbol representing the piece.
+     *
+     * @return The symbol representing the piece.
+     */
     public char getSymbol() {
         return color == Color.WHITE ? Character.toUpperCase(type.getSymbol()) : type.getSymbol();
     }
