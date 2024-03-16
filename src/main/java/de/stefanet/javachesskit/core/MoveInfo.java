c69b76cc004282033b8af18ddebb69cae8733207
Stefan Schulze <stefan.schulze@mdcc-fun.de>
2024-03-16 11:20:20 +0100

    Add Javadoc to MoveInfo.java

--- src/main/java/de/stefanet/javachesskit/core/MoveInfo.java
+++ src/main/java/de/stefanet/javachesskit/core/MoveInfo.java
@@ -2,6 +2,9 @@ package de.stefanet.javachesskit.core;
 
 import java.util.Objects;
 
+/**
+ * Represents additional information about a chess move.
+ */
 public class MoveInfo {
     private final Move move;
     private final Piece movedPiece;
@@ -13,6 +16,19 @@ public class MoveInfo {
     private final boolean isCheck;
     private final boolean isCheckmate;
 
+    /**
+     * Constructs a MoveInfo with the specified parameters.
+     *
+     * @param move              The move.
+     * @param movedPiece        The piece that was moved.
+     * @param capturedPiece     The piece that was captured (if any).
+     * @param san               The standard algebraic notation (SAN) of the move.
+     * @param isEnpassant       Whether the move is an en passant capture.
+     * @param isKingSideCastle  Whether the move is a kingside castle.
+     * @param isQueenSideCastle Whether the move is a queenside castle.
+     * @param isCheck           Whether the move puts the opponent's king in check.
+     * @param isCheckmate       Whether the move results in checkmate.
+     */
     public MoveInfo(Move move, Piece movedPiece, Piece capturedPiece, String san, boolean isEnpassant, boolean isKingSideCastle, boolean isQueenSideCastle, boolean isCheck, boolean isCheckmate) {
         this.move = move;
         this.movedPiece = movedPiece;
@@ -25,42 +41,93 @@ public class MoveInfo {
         this.isCheckmate = isCheckmate;
     }
 
+    /**
+     * Gets the move associated with this MoveInfo.
+     *
+     * @return The move.
+     */
     public Move getMove() {
         return move;
     }
 
+    /**
+     * Gets the piece that was moved.
+     *
+     * @return The moved piece.
+     */
     public Piece getMovedPiece() {
         return movedPiece;
     }
 
+    /**
+     * Gets the piece that was captured (if any).
+     *
+     * @return The captured piece, or null if no piece was captured.
+     */
     public Piece getCapturedPiece() {
         return capturedPiece;
     }
 
+    /**
+     * Gets the standard algebraic notation (SAN) of the move.
+     *
+     * @return The SAN of the move.
+     * @see <a href="https://www.chessprogramming.org/Algebraic_Chess_Notation">SAN</a>
+     */
     public String getSan() {
         return san;
     }
 
+    /**
+     * Checks if the move is an en passant capture.
+     *
+     * @return true if the move is an en passant capture, false otherwise.
+     */
     public boolean isEnpassant() {
         return isEnpassant;
     }
 
+    /**
+     * Checks if the move is a kingside castle.
+     *
+     * @return true if the move is a kingside castle, false otherwise.
+     */
     public boolean isKingSideCastle() {
         return isKingSideCastle;
     }
 
+    /**
+     * Checks if the move is a queenside castle.
+     *
+     * @return true if the move is a queenside castle, false otherwise.
+     */
     public boolean isQueenSideCastle() {
         return isQueenSideCastle;
     }
 
+    /**
+     * Checks if the move is a castle (either kingside or queenside).
+     *
+     * @return true if the move is a castle, false otherwise.
+     */
     public boolean isCastle() {
         return isKingSideCastle || isQueenSideCastle;
     }
 
+    /**
+     * Checks if the move puts the opponent's king in check.
+     *
+     * @return true if the move puts the opponent's king in check, false otherwise.
+     */
     public boolean isCheck() {
         return isCheck;
     }
 
+    /**
+     * Checks if the move results in checkmate.
+     *
+     * @return true if the move results in checkmate, false otherwise.
+     */
     public boolean isCheckmate() {
         return isCheckmate;
     }
