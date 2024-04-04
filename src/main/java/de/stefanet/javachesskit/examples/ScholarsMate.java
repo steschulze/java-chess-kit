e6e3ad48b4ce2801b98db1ac56a6b4e4c20223a6
Stefan Schulze <stefan.schulze@mdcc-fun.de>
2024-04-04 14:43:55 +0200

    Adjust ScholarsMate.java to new bitboard logic

--- src/main/java/de/stefanet/javachesskit/examples/ScholarsMate.java
+++ src/main/java/de/stefanet/javachesskit/examples/ScholarsMate.java
@@ -1,21 +1,21 @@
 package de.stefanet.javachesskit.examples;
 
 import de.stefanet.javachesskit.Move;
-import de.stefanet.javachesskit.board0x88.Position;
+import de.stefanet.javachesskit.bitboard.Board;
 
 
 public class ScholarsMate {
 
 	public static void main(String[] args) {
-		Position position = new Position();
-		position.makeMove(Move.fromUCI("e2e4"));
-		position.makeMove(Move.fromUCI("e7e5"));
-		position.makeMove(Move.fromUCI("d1h5"));
-		position.makeMove(Move.fromUCI("b8c6"));
-		position.makeMove(Move.fromUCI("f1c4"));
-		position.makeMove(Move.fromUCI("g8f6"));
-		position.makeMove(Move.fromUCI("h5f7"));
+		Board board = new Board();
+		board.push(Move.fromUCI("e2e4"));
+		board.push(Move.fromUCI("e7e5"));
+		board.push(Move.fromUCI("d1h5"));
+		board.push(Move.fromUCI("b8c6"));
+		board.push(Move.fromUCI("f1c4"));
+		board.push(Move.fromUCI("g8f6"));
+		board.push(Move.fromUCI("h5f7"));
 
-		System.out.println("Checkmate: " + position.isCheckmate());
+		System.out.println(board.outcome());
 	}
 }
