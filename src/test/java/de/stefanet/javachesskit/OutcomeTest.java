bb8b6f6652d69b3015fdb6f4684bd9ec59241d60
Stefan Schulze <stefan.schulze@mdcc-fun.de>
2024-04-04 18:33:21 +0200

    Add OutcomeTest.java with tests for three results

---
+++ src/test/java/de/stefanet/javachesskit/OutcomeTest.java
@@ -0,0 +1,43 @@
+package de.stefanet.javachesskit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class OutcomeTest {
+
+	@Test
+	void testResult_stalemate() {
+		Outcome outcome = new Outcome(Termination.STALEMATE, null);
+
+		assertEquals(Termination.STALEMATE, outcome.getTermination());
+		assertNull(outcome.getWinner());
+
+		assertEquals("1/2-1/2", outcome.result());
+		assertEquals("Outcome{termination=STALEMATE, winner=null}", outcome.toString());
+	}
+
+	@Test
+	void testResult_whiteWins() {
+		Outcome outcome = new Outcome(Termination.CHECKMATE, Color.WHITE);
+
+		assertEquals(Termination.CHECKMATE, outcome.getTermination());
+		assertEquals(Color.WHITE, outcome.getWinner());
+
+		assertEquals("1-0", outcome.result());
+		assertEquals("Outcome{termination=CHECKMATE, winner=WHITE}", outcome.toString());
+	}
+
+	@Test
+	void testResult_blackWins() {
+		Outcome outcome = new Outcome(Termination.CHECKMATE, Color.BLACK);
+
+		assertEquals(Termination.CHECKMATE, outcome.getTermination());
+		assertEquals(Color.BLACK, outcome.getWinner());
+
+		assertEquals("0-1", outcome.result());
+		assertEquals("Outcome{termination=CHECKMATE, winner=BLACK}", outcome.toString());
+	}
+
+}
